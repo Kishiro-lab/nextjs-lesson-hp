320d7b243d4ae394e77719d1325508789e5657d0
kishiro <the.yuhei@gmail.com>
2023-08-12 18:17:21 +0900

    initial-commit

---
+++ pages/contact-page.js
@@ -0,0 +1,89 @@
+import Layout from "../components/Layout";
+import Image from "next/image";
+
+const Contact = () => {
+  return (
+    <Layout title="Contact">
+      <div className="bg-white text-center shadow-xl p-8 w-80 rounded">
+        <div className="mt-4">
+          <p className="font-bold">Contact Info</p>
+        </div>
+        <div className="flex justify-center mt-4">
+          <Image
+            className="rounded-full"
+            src="/avatar.jpg"
+            width={60}
+            height={60}
+            alt="Avatar"
+          />
+        </div>
+        <div className="mt-4">
+          <p className="font-bold">Address</p>
+          <p className="text-xs mt-2 text-gray-600">city A</p>
+          <p className="font-bold mt-3">E-mail</p>
+          <p className="text-xs mt-2 text-gray-600">abc@gmail.com</p>
+          <p className="font-bold mt-3">Phone</p>
+          <p className="text-xs mt-2 text-gray-600">000-123-456</p>
+        </div>
+        <div className="mt-6 flex justify-around">
+          <div>
+            <a href="https://gist.github.com/mbostock/3094619">
+              <svg
+                className="w-6 h-6 mr-3 text-blue-500"
+                fill="currentColor"
+                viewBox="328 355 335 276"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="
+    M 630, 425
+    A 195, 195 0 0 1 331, 600
+    A 142, 142 0 0 0 428, 570
+    A  70,  70 0 0 1 370, 523
+    A  70,  70 0 0 0 401, 521
+    A  70,  70 0 0 1 344, 455
+    A  70,  70 0 0 0 372, 460
+    A  70,  70 0 0 1 354, 370
+    A 195, 195 0 0 0 495, 442
+    A  67,  67 0 0 1 611, 380
+    A 117, 117 0 0 0 654, 363
+    A  65,  65 0 0 1 623, 401
+    A 117, 117 0 0 0 662, 390
+    A  65,  65 0 0 1 630, 425
+    Z"
+                />
+              </svg>
+            </a>
+          </div>
+          <div>
+            <a href="https://gist.github.com/levinmejia/fad2d6fe56c75d577c9a">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                version="1.0"
+                viewBox="0 0 50 50"
+                className="w-6 h-6 mr-3 text-blue-700"
+                fill="currentColor"
+              >
+                <path d="M40,0H10C4.486,0,0,4.486,0,10v30c0,5.514,4.486,10,10,10h30c5.514,0,10-4.486,10-10V10C50,4.486,45.514,0,40,0z M39,17h-3 c-2.145,0-3,0.504-3,2v3h6l-1,6h-5v20h-7V28h-3v-6h3v-3c0-4.677,1.581-8,7-8c2.902,0,6,1,6,1V17z"></path>
+              </svg>
+            </a>
+          </div>
+          <div>
+            <a href="https://primer.style/design/foundations/icons/mark-github-16">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                className="w-6 h-6 mr-3 text-gray-500"
+                fill="currentColor"
+              >
+                <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Contact;
